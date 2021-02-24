826cbf9373b7932f4b9db47c588354b5950f9a7a
XON 001 <hamidxonanvarxonov@gmail.com>
2021-02-23 23:55:44 -0800

    intial

---
+++ Code.py
@@ -0,0 +1,20 @@
+class mani:
+    def init(self, pul):
+        self.money = None
+
+    def balns(self):
+        print(self.money)
+
+    def depozit(self):
+        x = int(input(" depozit"))
+        self.money = x
+
+    def yechish(self):
+        x = int(input(" withdraw"))
+        self.money
+class sde(mani):
+    def kll(self, x):
+        super().init(pul)
+
+    def Mny(self):
+#         return self.x * 1.04
---
+++ main.py
@@ -0,0 +1,19 @@
+#1######################
+class Vehicle:
+  def __init__(self, Max_speed, mileage):
+    self.Max_speed = Max_speed
+    self.mileage = mileage
+
+p1 = Vehicle()
+
+print(p1.Max_speed)
+print(p1.mileage)
+
+#2#####################
+class Abs:
+    def __init__(self):
+        pass
+#3#####################
+
+
+
